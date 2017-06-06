ed8c7887f56500576aca15884ed027ce1a367265
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-06-06 16:23:00 -0600

    Added Proposal class

---
+++ freelance-camp-fe/app/proposal/proposal.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Proposal {
+    constructor(id, customer, portfolio_url = 'http://', tools, estimated_hours, hourly_rate, week_to_complete, client_email) {
+        this.id = id;
+        this.customer = customer;
+        this.portfolio_url = portfolio_url;
+        this.tools = tools;
+        this.estimated_hours = estimated_hours;
+        this.hourly_rate = hourly_rate;
+        this.week_to_complete = week_to_complete;
+        this.client_email = client_email;
+    }
+}
+exports.Proposal = Proposal;
+// 15, 'Abc Company', 'http://portfolio.aaronanderson.com', 'Ruby on Rails', 150, 120, 15, 'aaronanderson@example.com'
+//# sourceMappingURL=proposal.js.map
